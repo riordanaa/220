20275e83cade96215e9c7862d8b9c613faa300a7
aidan <riordanaa@g.cofc.edu>
2021-10-06 23:32:47 -0400

    Completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,11 @@
+'''
+Name: <Aidan Riordan>
+program name: <hello.py>
+problem: saying hello to the world
+I Aidan Riordan certify that this is my own work
+'''
+
+def main():
+    print("hello, world!")
+
+main()
