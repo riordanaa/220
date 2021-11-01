275e0ad896869d1bcc32899454d0ddb3ec7fc17a
aidan <riordanaa@g.cofc.edu>
2021-10-31 20:40:33 -0400

    Completed assignment 6 Vigenere

--- assignments/hw6/vigenere.py
+++ assignments/hw6/vigenere.py
@@ -6,6 +6,7 @@ I Aidan Riordan certify that this is my own work
 
 from graphics import *
 
+
 def main():
     win = GraphWin("greeting card", 500, 500)
     win.setCoords(0, 0, 100, 100)
@@ -13,13 +14,13 @@ def main():
     text.draw(win)
     text2 = Text(Point(20, 70), "Enter Keyword")
     text2.draw(win)
-    input_text1 = Entry(Point(70,90), 20)
-    input_text2 = Entry(Point(70,70),10)
+    input_text1 = Entry(Point(70, 90), 20)
+    input_text2 = Entry(Point(70, 70), 10)
     input_text1.draw(win)
     input_text2.draw(win)
 
-    button = Text(Point(70,60), "Encode")
-    rectangle = Rectangle(Point(60,55), Point(80,65))
+    button = Text(Point(70, 60), "Encode")
+    rectangle = Rectangle(Point(60, 55), Point(80, 65))
     rectangle.draw(win)
     button.draw(win)
     win.getMouse()
@@ -30,23 +31,20 @@ def main():
     encoded = code(message, keyword)
     button.setText("Resulting message" + "\n" + encoded + "\n" + "\n" + "Click anywhere to close window")
 
-    time.sleep(4)
     win.getMouse()
     win.close()
 
+
 def code(message, keyword):
-    #format to upper and no spaces
     message = message.upper()
     message = message.replace(" ", "")
-    #print(message)
     keyword = keyword.upper()
-    keyword = keyword.replace(" ","")
-
+    keyword = keyword.replace(" ", "")
     # adjust length of key accordingly
     len_dif = len(message) - len(keyword)
+    remainder = len_dif % len(keyword)
     keyword = keyword * (len_dif + 1)
-
-    #convert to lists and ords
+    keyword = keyword + keyword[0:remainder]
     list_key = []
     list_mes = []
     encoded_message = ""
@@ -56,9 +54,10 @@ def code(message, keyword):
     for letter in keyword:
         list_key.append(ord(letter))
     for val in list_mes:
-        x = (val + list_key[i]) %26 + ord("A")
+        x = (val + list_key[i]) % 26 + ord("A")
         encoded_message = encoded_message + str(chr(x))
         i = i + 1
+
     print(encoded_message)
     return encoded_message
 
