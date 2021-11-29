4f3ee29c5cd5fe782c3c568af98edc7dfb0c5c7f
aidan <riordanaa@g.cofc.edu>
2021-11-28 21:44:50 -0500

    lab4

--- labs/lab4/lab4.py
+++ labs/lab4/lab4.py
@@ -1,10 +1,9 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Aidan Riordan>
+<Lab4>.py
 """
-
 from graphics import *
-
+from math import *
 
 def squares():
     """  <---  You can use tripled quotes to write a multi-line comment....
@@ -24,41 +23,29 @@ def squares():
     width = 400
     height = 400
     win = GraphWin("Lab 4", width, height)
-
     # number of times user can move circle
     num_clicks = 5
-
     # create a space to instruct user
     inst_pt = Point(width / 2, height - 10)
-    instructions = Text(inst_pt, "Click to move circle")
+    instructions = Text(inst_pt, "Click to make square")
     instructions.draw(win)
-
-    # builds a circle
-    shape = Circle(Point(50, 50), 20)
+    # builds a square
+    shape = Rectangle(Point(50, 50), Point(70, 70))
     shape.setOutline("red")
     shape.setFill("red")
     shape.draw(win)
-
     # allows the user to click multiple times to move the circle
     for i in range(num_clicks):
-        p = win.getMouse()
-        c = shape.getCenter()  # center of circle
-
-        # move amount is distance from center of circle to the
-        # point where the user clicked
-        #dx = p.getX() - c.getX()
-        #dy = p.getY() - c.getY()
-        #shape.move(dx, dy)
-        new_shape = Circle(Point(p.getX(), p.getY()), 20)
-        new_shape.setOutline("red")
-        new_shape.setFill("red")
-        new_shape.draw(win)
-        instructions = Text(inst_pt, "Click again to quit")
-        instructions.draw(win)
-
+        clicked = win.getMouse()
+        s = Rectangle(Point(clicked.x - 10, clicked.y - 10), Point(clicked.x + 10, clicked.y + 10))
+        s.setOutline("red")
+        s.setFill("red")
+        s.draw(win)
+    point_message = Point(width / 2, height - 25)
+    message = Text(point_message, "Click again to quit")
+    message.draw(win)
     win.getMouse()
     win.close()
-
 
 def rectangle():
     """
@@ -68,14 +55,56 @@ def rectangle():
          Print the perimeter and area of the rectangle.
     Formulas: area = (length)(width)   and    perimeter = 2(length+width)
     """
-    pass
+    win = GraphWin("Lab 4", 400, 400)
+    Text(Point(250, 390), "Click the two opposite corners of the rectangle").draw(win)
+    p1 = win.getMouse()
+    x1 = p1.getX()
+    y1 = p1.getY()
+    p2 = win.getMouse()
+    x2 = p2.getX()
+    y2 = p2.getY()
+    shape = Rectangle(Point(x1, y1), Point(x2, y2))
+    shape.setFill("blue")
+    shape.draw(win)
+    Text(Point(150, 50), "The area is: " + str((abs(x2 - x1)) / (abs(y2-y1)))).draw(win)
+    win.getMouse()
+    win.close()
 
+def circle():
+    win = GraphWin("Lab 4", 400, 400)
+    # instruction
+    inst_pt = Point(200, 380)
+    Text(inst_pt, "Click once to get center of circle and click again to get the outer point").draw(win)
+    p1 = win.getMouse()
+    x1 = p1.getX()
+    y1 = p1.getY()
+    p2 = win.getMouse()
+    x2 = p2.getX()
+    y2 = p2.getY()
+    radius = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    shape = Circle(p1, radius)
+    shape.setFill("red")
+    shape.draw(win)
+    txt = Text(Point(150, 70), "The radius is " + str(radius))
+    txt.draw(win)
+    win.getMouse()
+    win.close()
+
+def pi2():
+    n = eval(input("How many terms do you want to use in the approximation?"))
+    acc = 0
+    for i in range(n):
+        numerator = 4
+        denominator = 1 + (2*i)
+        ratio = (numerator / denominator) * ((-1)**i)
+        acc = acc + ratio
+    print(acc)
 
 def main():
-    squares()
-    # rectangle()
-    # circle()
-    # pi2()
+    # squares()
+    #rectangle()
+    #circle()
+    pi2()
 
 
 main()
