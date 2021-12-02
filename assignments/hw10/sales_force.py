79670077b5968e449a030b90a1a1c261db816a7c
aidan <riordanaa@g.cofc.edu>
2021-12-02 17:18:23 -0500

    lab13

---
+++ assignments/hw10/sales_force.py
@@ -0,0 +1,78 @@
+"""
+Name: <Aidan Riordan>
+program name: <button.py>
+I Aidan Riordan certify that this is my own work
+"""
+from sales_person import SalesPerson
+class SalesForce:
+
+    def __init__(self):
+        self.sales_people = []
+
+    def add_data(self, file_name):
+        file = open(file_name, "r")
+        for line in file.readlines():
+            employee_id, name, sales = line.split(",")
+            unique_object_name = "employee" + str(employee_id)
+            unique_object_name = SalesPerson(employee_id, name)
+            sales = sales.split()
+            for sale in sales:
+                unique_object_name.enter_sale(sale)
+            self.sales_people.append(unique_object_name)
+        file.close()
+
+    def quota_report(self, quota):
+        list = []
+        for person in self.sales_people:
+            sublist = [int(person.get_id()), person.get_name(), person.total_sales(), person.met_quota(quota)]
+            list.append(sublist)
+        return list
+
+    def top_seller(self):
+        people_highest_total_sales = []
+        total_sales = []
+        for person in self.sales_people:
+            if not(person.total_sales()):
+                total_sales.append(0)
+            else:
+                total_sales.append(person.total_sales())
+        highest_total = 0
+        for i in range(len(total_sales)):
+            if total_sales[i] >= highest_total:
+                people_highest_total_sales.append(self.sales_people[i])
+        return people_highest_total_sales
+
+
+
+
+
+    def individual_sales(self, employee_id):
+        for person in self.sales_people:
+            if person.get_id() == employee_id:
+                return person
+        return None
+
+
+
+        # do i need to convert to int or string here?? line 26
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def main:
+#if __name__ == '__main__':
+    #main()
---
+++ assignments/hw10/sales_person.py
@@ -0,0 +1,70 @@
+class SalesPerson:
+    """This class contains employee data on a respective sales person"""
+
+    def __init__(self, employee_id, name):
+        """Initializes the sales person with respective employee_id and name"""
+        self.employee_id = int(employee_id)
+        self.name = name
+        self.sales = []
+
+    def get_id(self):
+        return self.employee_id
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def enter_sale(self, sale):
+        self.sales.append(sale)
+
+    def total_sales(self):
+        acc = 0
+        for sale in self.sales:
+            acc = acc + sale
+        return acc
+
+    def get_sales(self):
+        return self.sales
+
+    def met_quota(self, quota):
+        if self.total_sales() >= quota: # When do we put () to call a function? allways!
+            return True
+        return False
+
+    def compare_to(self, other):
+        if other.total_sales() > self.total_sales():
+            return -1
+        if self.total_sales() > other.total_sales():
+            return 1
+        return 0
+
+
+    def __str__(self):
+        x = "{0}-{1}:{2}".format(str(self.get_id()), self.get_name(), str(self.total_sales()))
+        return x
+
+
+
+
+
+
+
+
+
+
+#note when do we need to change the data types in order to pass the tests??
+# when they are entered? when they are got by getters?
+
+# on line 24, do you want me to call it from the instance variable or from the getter function? This reference is being
+#made in a class function.
+
+
+
+#below testing the string formatting
+"""employee_1 = SalesPerson(201, "aidan")
+print(employee_1.get_name())
+employee_1.enter_sale(10)
+employee_1.enter_sale(20)
+print(employee_1.__str__())"""
