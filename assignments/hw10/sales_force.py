1656c3238fd9cb30e0bbc81ef6314e993703b7c7
aidan <riordanaa@g.cofc.edu>
2021-12-02 17:57:19 -0500

    HW 10 TEMP

--- assignments/hw10/sales_force.py
+++ assignments/hw10/sales_force.py
@@ -1,10 +1,13 @@
 """
 Name: <Aidan Riordan>
-program name: <button.py>
+program name: <salesforce.py>
 I Aidan Riordan certify that this is my own work
 """
 from sales_person import SalesPerson
+
+
 class SalesForce:
+    """This class manages a salesforce"""
 
     def __init__(self):
         self.sales_people = []
@@ -22,17 +25,17 @@ class SalesForce:
         file.close()
 
     def quota_report(self, quota):
-        list = []
+        list_a = []
         for person in self.sales_people:
             sublist = [int(person.get_id()), person.get_name(), person.total_sales(), person.met_quota(quota)]
-            list.append(sublist)
-        return list
+            list_a.append(sublist)
+        return list_a
 
     def top_seller(self):
         people_highest_total_sales = []
         total_sales = []
         for person in self.sales_people:
-            if not(person.total_sales()):
+            if not person.total_sales():
                 total_sales.append(0)
             else:
                 total_sales.append(person.total_sales())
@@ -42,37 +45,9 @@ class SalesForce:
                 people_highest_total_sales.append(self.sales_people[i])
         return people_highest_total_sales
 
-
-
-
-
     def individual_sales(self, employee_id):
         for person in self.sales_people:
             if person.get_id() == employee_id:
                 return person
         return None
-
-
-
         # do i need to convert to int or string here?? line 26
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#def main:
-#if __name__ == '__main__':
-    #main()
--- assignments/hw10/sales_person.py
+++ assignments/hw10/sales_person.py
@@ -1,3 +1,8 @@
+"""
+Name: <Aidan Riordan>
+program name: <salesperson.py>
+I Aidan Riordan certify that this is my own work
+"""
 class SalesPerson:
     """This class contains employee data on a respective sales person"""
 
@@ -29,7 +34,7 @@ class SalesPerson:
         return self.sales
 
     def met_quota(self, quota):
-        if self.total_sales() >= quota: # When do we put () to call a function? allways!
+        if self.total_sales() >= quota:
             return True
         return False
 
@@ -40,31 +45,9 @@ class SalesPerson:
             return 1
         return 0
 
-
     def __str__(self):
-        x = "{0}-{1}:{2}".format(str(self.get_id()), self.get_name(), str(self.total_sales()))
-        return x
+        return "{0}-{1}:{2}".format(str(self.get_id()), self.get_name(), str(self.total_sales()))
 
-
-
-
-
-
-
-
-
-
-#note when do we need to change the data types in order to pass the tests??
+# note when do we need to change the data types in order to pass the tests?
 # when they are entered? when they are got by getters?
-
-# on line 24, do you want me to call it from the instance variable or from the getter function? This reference is being
-#made in a class function.
-
-
-
-#below testing the string formatting
-"""employee_1 = SalesPerson(201, "aidan")
-print(employee_1.get_name())
-employee_1.enter_sale(10)
-employee_1.enter_sale(20)
-print(employee_1.__str__())"""
+# made in a class function.
